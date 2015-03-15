5641b6ae404ac8d385a201efdfcdec8833b7f87b
soutchay <chungvus@gmail.com>
2015-03-14 21:05:27 -0700

    refactor beer_spec

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe Beer do
   it 'has a name' do
-    beer = Beer.create name: 'Heineken'
+    beer = described_class.create name: 'Heineken'
 
     expect(beer.name).to eq('Heineken')
   end
   it 'requires a name' do
-    beer = Beer.new
+    beer = described_class.new
 
     expect(beer.valid?).to eq(false)
     expect(beer.errors).to include(:name)
