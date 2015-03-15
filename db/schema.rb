7df0206a485094152612cfda740521aa2480bd74
soutchay <chungvus@gmail.com>
2015-03-14 20:42:00 -0700

    switch to postgresql from sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150315033439) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "beers", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
