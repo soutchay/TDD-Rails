926b4ea9fb8dc324a2c8c9bbbc0d3abbb25f9e27
soutchay <chungvus@gmail.com>
2015-03-14 21:02:21 -0700

    test to make sure beer has a name

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 describe Beer do
   it 'has a name' do
     beer = Beer.create name: 'Heineken'
+
     expect(beer.name).to eq('Heineken')
+  end
+  it 'requires a name' do
+    beer = Beer.new
+
+    expect(beer.valid?).to eq(false)
+    expect(beer.errors).to include(:name)
   end
 end
